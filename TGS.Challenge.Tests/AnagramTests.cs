22d9b87ab1bad793201cf189b47df9f9ad4c4aba
Liall Jordaan <liall.jordaan@gmail.com>
2021-05-31 00:27:42 +0200

    Added Anagram challenge code and updated tests

--- TGS.Challenge.Tests/AnagramTests.cs
+++ TGS.Challenge.Tests/AnagramTests.cs
@@ -3,66 +3,84 @@ using NUnit.Framework;
 
 namespace TGS.Challenge.Tests
 {
-  [TestFixture()]
-  public class AnagramTests
-  {
-    private readonly Anagram _anagram;
-
-    public AnagramTests()
+    [TestFixture()]
+    public class AnagramTests
     {
-      this._anagram = new Anagram();
+        private readonly Anagram _anagram;
+
+        public AnagramTests()
+        {
+            this._anagram = new Anagram();
+        }
+
+        [Test()]
+        public void Word1_IsRequired()
+        {
+            var ex = Assert.Throws<ArgumentException>(() => _anagram.AreAnagrams(string.Empty, "ABC"));
+            Assert.That(ex.Message, Is.EqualTo("Word1 cannot be null. Please enter a valid word."));
+        }
+
+        [Test()]
+        public void Word2_IsRequired()
+        {
+            var ex = Assert.Throws<ArgumentException>(() => _anagram.AreAnagrams("ABC", string.Empty));
+            Assert.That(ex.Message, Is.EqualTo("Word2 cannot be null. Please enter a valid word."));
+        }
+
+        [Test()]
+        public void Dormitory_IsAnagram_Dirty_room()
+        {
+            var result = _anagram.AreAnagrams("Dormitory", "Dirty_room");
+
+            Assert.IsTrue(result);
+        }
+
+        [Test()]
+        public void Funeral_IsAnagram_Reel_fun()
+        {
+            var result = _anagram.AreAnagrams("Funeral", "Reel fun");
+
+            Assert.IsFalse(result);
+        }
+
+        [Test()]
+        public void School_master_IsAnagram_The_classroom()
+        {
+            var result = _anagram.AreAnagrams("School master?!", "!?The classroom");
+
+            Assert.IsTrue(result);
+        }
+
+        [Test()]
+        public void Listen_Is_NOT_Anagram_Silence()
+        {
+            var result = _anagram.AreAnagrams("Listen", "Silence");
+
+            Assert.IsFalse(result);
+        }
+
+        [Test()]
+        public void Funeral_IsAnagram_Real_fun()
+        {
+            var result = _anagram.AreAnagrams("Funeral", "Real fun");
+
+            Assert.IsTrue(result);
+        }
+
+        [Test()]
+        public void IsAnagram_Contains_Same_Numbers()
+        {
+            var result = _anagram.AreAnagrams("1 Funeral", "Realfun 1");
+
+            Assert.IsTrue(result);
+        }
+
+        [Test()]
+        public void IsAnagram_Contains_Different_Numbers()
+        {
+            var result = _anagram.AreAnagrams("1 Funeral", "Real fun 2");
+
+            Assert.IsFalse(result);
+        }
     }
-
-    [Test()]
-    public void Word1_IsRequired()
-    {
-      Assert.Throws<ArgumentException>(() => _anagram.AreAnagrams(string.Empty, "ABC"));
-    }
-
-    [Test()]
-    public void Word2_IsRequired()
-    {
-      Assert.Throws<ArgumentException>(() => _anagram.AreAnagrams("ABC", string.Empty));
-    }
-
-    [Test()]
-    public void Dormitory_IsAnagram_Dirty_room()
-    {
-      var result = _anagram.AreAnagrams("Dormitory", "Dirty_room");
-
-      Assert.IsTrue(result);
-    }
-
-    [Test()]
-    public void Funeral_IsAnagram_Reel_fun()
-    {
-      var result = _anagram.AreAnagrams("Funeral", "Reel fun");
-
-      Assert.IsTrue(result);
-    }
-
-    [Test()]
-    public void School_master_IsAnagram_The_classroom()
-    {
-      var result = _anagram.AreAnagrams("School master?!", "!?The classroom");
-
-      Assert.IsTrue(result);
-    }
-    
-    [Test()]
-    public void Listen_Is_NOT_Anagram_Silence()
-    {
-      var result = _anagram.AreAnagrams("Listen", "Silence");
-
-      Assert.IsFalse(result);
-    }
-
-    [Test()]
-    public void Funeral_IsAnagram_Real_fun()
-    {
-      var result = _anagram.AreAnagrams("Funeral", "Real fun");
-
-      Assert.IsTrue(result);
-    }
-  }
 }
--- TGS.Challenge/Anagram.cs
+++ TGS.Challenge/Anagram.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Text.RegularExpressions;
+
 namespace TGS.Challenge
 {
   /*
@@ -22,9 +25,42 @@ namespace TGS.Challenge
      */
     public class Anagram
     {
-      public bool AreAnagrams(string word1, string word2)
-      {
-        return false;
-      }
+        public bool AreAnagrams(string word1, string word2)
+        {
+            var alphaWord1 = IgnorePunctuation(word1.ToLowerInvariant());
+            var alphaWord2 = IgnorePunctuation(word2.ToLowerInvariant());
+
+            if(string.IsNullOrWhiteSpace(alphaWord1))
+            {
+                throw new System.ArgumentException("Word1 cannot be null. Please enter a valid word.");
+            }
+
+            if (string.IsNullOrWhiteSpace(alphaWord2))
+            {
+                throw new System.ArgumentException("Word2 cannot be null. Please enter a valid word.");
+            }
+
+            var arrAlphaWord1 = alphaWord1.ToCharArray();
+            var arrAlphaWord2 = alphaWord2.ToCharArray();
+
+            Array.Sort(arrAlphaWord1);
+            Array.Sort(arrAlphaWord2);
+
+            var sortedWord1 = new string(arrAlphaWord1);
+            var sortedWord2 = new string(arrAlphaWord2);
+
+            if(sortedWord1 == sortedWord2)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        private string IgnorePunctuation(string word)
+        {
+            //could also be more explicit and use Regex.Replace(word, "[^a-zA-Z0-9]+", "") but \W_ excludes everything that non alphanumeric characters including underscores
+            return Regex.Replace(word, @"[\W_]", string.Empty);
+        }
     }
 }
